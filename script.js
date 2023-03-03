4243faf0b5f57852eab4129942241727aaa05911
Rizan012 <rcesstha@gmail.com>
2023-03-03 08:24:36 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function validaton()
+{
+    var name=document.getElementById('name').value;
+    var address=document.getElementById('address').value;
+    //alert(name+' '+address);
+    if (name=='')
+    {
+        alert('Name is required');
+        document.getElementById('name').focus();
+    }
+    if (address=='')
+    {
+        alert('Address is required');
+    }
+
+}
+
+document.getElementById("myform"),addEventListener("submit",validaton);
